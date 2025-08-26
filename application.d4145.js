d02ab7b6a9a79e4b3085224e16e1a40b9377b143
上帝的梦 <1597691532@qq.com>
2025-08-26 11:47:21 +0800

    1

--- application.d4145.js
+++ application.d4145.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.96d56.json';
+          this.settingsPath = 'src/settings.c0fb2.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.fca50.js
+++ index.fca50.js
@@ -1,4 +1,4 @@
-System.register(["./application.361b5.js"], function (_export, _context) {
+System.register(["./application.d4145.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
