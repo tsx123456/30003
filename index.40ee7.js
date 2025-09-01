499700ee0a691a2721281998f4478b8afaef9553
上帝的梦 <1597691532@qq.com>
2025-09-01 16:34:01 +0800

    1

--- index.40ee7.js
+++ index.40ee7.js
@@ -1,4 +1,4 @@
-System.register(["./application.75ba7.js"], function (_export, _context) {
+System.register(["./application.a1d1c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
