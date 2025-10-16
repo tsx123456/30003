798d81e2ca610d3af170355722f2b0474e70075a
上帝的梦 <1597691532@qq.com>
2025-10-16 16:09:18 +0800

    1

--- application.dda78.js
+++ application.dda78.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.d6547.json';
+          this.settingsPath = 'src/settings.69165.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
